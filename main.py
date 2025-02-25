247d2c21b0ac513ad6d9997f97a9b8fb63c87675
Nick Pisarski <nick.pisarski@slalom.com>
2025-02-25 15:54:07 -0600

    Add main entry point for expense analyzer

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Main entry point for the expense analyzer"""
+
+
+def main():
+    """Main entry point for the expense analyzer"""
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
